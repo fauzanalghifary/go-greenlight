daed26ecbc94d6b10bedeea3c9dd3458bc87a1b0
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-02-22 10:09:49 +0700

    Finish Chapter 13.3 Sending a Welcome Email

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"github.com/fauzanalghifary/go-greenlight/internal/data"
+	"github.com/fauzanalghifary/go-greenlight/internal/mailer"
 	_ "github.com/lib/pq"
 	"log/slog"
 	"os"
@@ -27,12 +28,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *slog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -54,6 +63,12 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "7c5348e88d3fbd", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "217ac26657486d", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
@@ -72,6 +87,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
