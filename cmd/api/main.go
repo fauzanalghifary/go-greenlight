90ac1b3e028f78cea3e93147672df0b2465d889b
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-02-28 20:27:08 +0700

    Finish Chapter 17.3 Simple CORS Requests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/lib/pq"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
@@ -35,6 +36,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -70,6 +74,13 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "7c5348e88d3fbd", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "217ac26657486d", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
+	flag.Func(
+		"cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+			cfg.cors.trustedOrigins = strings.Fields(val)
+			return nil
+		},
+	)
 
 	flag.Parse()
 
