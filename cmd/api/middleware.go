fb1573bef141ff694349734958621a4772fa0d5e
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-03-03 09:38:41 +0700

    Finish Chapter 19.4 Module Proxies and Vendoring

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"github.com/fauzanalghifary/go-greenlight/internal/data"
 	"github.com/fauzanalghifary/go-greenlight/internal/validator"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -59,11 +59,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			if app.config.limiter.enabled {
-				ip, _, err := net.SplitHostPort(r.RemoteAddr)
-				if err != nil {
-					app.serverErrorResponse(w, r, err)
-					return
-				}
+				ip := realip.FromRequest(r)
 
 				mu.Lock()
 
