bf011395f5957a71ead7915b5f2278a234b44352
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-03-03 08:25:25 +0700

    Finish Chapter 18.2 Creating Custom Metrics

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"github.com/fauzanalghifary/go-greenlight/internal/data"
 	"github.com/fauzanalghifary/go-greenlight/internal/mailer"
 	_ "github.com/lib/pq"
 	"log/slog"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -95,6 +97,32 @@ func main() {
 	defer db.Close()
 
 	logger.Info("database connection pool established")
+
+	expvar.NewString("version").Set(version)
+
+	expvar.Publish(
+		"goroutines", expvar.Func(
+			func() any {
+				return runtime.NumGoroutine()
+			},
+		),
+	)
+
+	expvar.Publish(
+		"database", expvar.Func(
+			func() any {
+				return db.Stats()
+			},
+		),
+	)
+
+	expvar.Publish(
+		"timestamp", expvar.Func(
+			func() any {
+				return time.Now().Unix()
+			},
+		),
+	)
 
 	app := &application{
 		config: cfg,
