08203b97dd389ed6dae489c3d9905b5d659a11bf
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-03-03 08:48:21 +0700

    Finish Chapter 18.4 Recording HTTP Status Codes

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -9,6 +9,7 @@ import (
 	"golang.org/x/time/rate"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -218,11 +219,48 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	)
 }
 
+type metricsResponseWriter struct {
+	wrapped       http.ResponseWriter
+	statusCode    int
+	headerWritten bool
+}
+
+func newMetricsResponseWriter(w http.ResponseWriter) *metricsResponseWriter {
+	return &metricsResponseWriter{
+		wrapped:    w,
+		statusCode: http.StatusOK,
+	}
+}
+
+func (mw *metricsResponseWriter) Header() http.Header {
+	return mw.wrapped.Header()
+}
+
+func (mw *metricsResponseWriter) WriteHeader(statusCode int) {
+	mw.wrapped.WriteHeader(statusCode)
+
+	if !mw.headerWritten {
+		mw.statusCode = statusCode
+		mw.headerWritten = true
+	}
+}
+
+func (mw *metricsResponseWriter) Write(b []byte) (int, error) {
+	mw.headerWritten = true
+	return mw.wrapped.Write(b)
+}
+
+func (mw *metricsResponseWriter) Unwrap() http.ResponseWriter {
+	return mw.wrapped
+}
+
 func (app *application) metrics(next http.Handler) http.Handler {
 	var (
 		totalRequestsReceived           = expvar.NewInt("total_requests_received")
 		totalResponsesSent              = expvar.NewInt("total_responses_sent")
 		totalProcessingTimeMicroseconds = expvar.NewInt("total_processing_time_μs")
+
+		totalResponsesSentByStatus = expvar.NewMap("total_responses_sent_by_status")
 	)
 
 	return http.HandlerFunc(
@@ -231,9 +269,13 @@ func (app *application) metrics(next http.Handler) http.Handler {
 
 			totalRequestsReceived.Add(1)
 
-			next.ServeHTTP(w, r)
+			mw := newMetricsResponseWriter(w)
+
+			next.ServeHTTP(mw, r)
 
 			totalResponsesSent.Add(1)
+
+			totalResponsesSentByStatus.Add(strconv.Itoa(mw.statusCode), 1)
 
 			duration := time.Since(start).Microseconds()
 			totalProcessingTimeMicroseconds.Add(duration)
